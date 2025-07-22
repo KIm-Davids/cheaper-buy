5cace19bf9babd7253caeff96b4e112e60149d3d
Kimberly Davids <kimdhaviex1@gmail.com>
2025-07-22 13:59:21 +0100

    In Progress: Added cors config

--- src/main/java/com/packages/scraperapi/config/WebConfig.java
+++ src/main/java/com/packages/scraperapi/config/WebConfig.java
@@ -2,23 +2,24 @@ package com.packages.scraperapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://smartpriceng.vercel.app")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-                System.out.println("CORS configuration applied");
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://smartpriceng.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
 
-            }
-        };
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
