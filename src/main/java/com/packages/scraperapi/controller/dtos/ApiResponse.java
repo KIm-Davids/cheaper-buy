3710880b09c3b289892604b6569139c31cb45104
Kimberly Davids <kimdhaviex1@gmail.com>
2025-08-13 17:03:14 +0100

    Fixed: Controller

--- src/main/java/com/packages/scraperapi/controller/dtos/ApiResponse.java
+++ src/main/java/com/packages/scraperapi/controller/dtos/ApiResponse.java
@@ -1,10 +1,23 @@
 package com.packages.scraperapi.controller.dtos;
 
+import com.packages.scraperapi.models.ProductResult;
 import lombok.AllArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 public class ApiResponse {
     public boolean isLoaded;
-    public Object data;
+    public List<ProductResult> data;
+    public Object response;
 
+    public ApiResponse(boolean b, List<ProductResult> response) {
+        this.isLoaded = b;
+        this.data = response;
+    }
+
+    public ApiResponse(boolean b, Object response) {
+        this.isLoaded = b;
+        this.response = response;
+    }
 }
